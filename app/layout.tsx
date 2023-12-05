b4b8c0a0320fd45f5254f5d457407e6ee295fdeb
valberjr <github@jvscs.maskmy.id>
2023-12-05 14:43:29 -0300

    clerk authentication added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -17,13 +18,15 @@ export default function RootLayout({
   readonly children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
-      <body className={`h-full ${inter.className}`}>
-        <div className="flex flex-col h-full mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
-          {children}
-        </div>
-        <Analytics />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className="h-full">
+        <body className={`h-full ${inter.className}`}>
+          <div className="flex flex-col h-full mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+            {children}
+          </div>
+          <Analytics />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
