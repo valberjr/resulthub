d6448ffe7f378383c21c4e228118b15e0a514d2d
valberjr <github@jvscs.maskmy.id>
2023-12-05 13:38:36 -0300

    vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
