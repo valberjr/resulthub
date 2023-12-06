e6c64cf1ea704fac785259b1c48f343fa80d90b8
valberjr <github@jvscs.maskmy.id>
2023-12-06 09:23:07 -0300

    navbar removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import Navbar from '@/components/ui/Navbar';
 import '@/styles/globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider, UserButton } from '@clerk/nextjs';
+
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -21,9 +21,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className}`}>
-          <Navbar />
-          <div className="">{children}</div>
+        <body className={`${inter.className} min-h-screen bg-gray-100`}>
+          <div className="flex">
+            <div className="flex flex-col justify-end items-center w-1/4 lg:w-1/5 bg-gray-900 min-h-screen p-4">
+              <UserButton afterSignOutUrl="/" />
+            </div>
+            <main className="flex-1">
+              <div className="mx-auto max-w-7xl p-4">{children}</div>
+            </main>
+          </div>
           <Analytics />
         </body>
       </html>
